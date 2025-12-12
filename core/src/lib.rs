8e75ae7c723e197b7b03475513e6eb148798518f
Marvin Dziedzina <marvin.dziedzina@protonmail.com>
2025-12-12 15:21:42 +0100

    Restart with dioxus

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,13 +1,1 @@
-use bevy::prelude::*;
 
-use crate::{osm::OSMPlugin, osm_map::OSMMapPlugin};
-
-mod osm_map;
-
-pub struct FOSSTAKCorePlugin;
-
-impl Plugin for FOSSTAKCorePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(OSMMapPlugin);
-    }
-}
